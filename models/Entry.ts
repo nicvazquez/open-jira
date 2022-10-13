a688b5b2e7bd224499e4e5f0bb57c43a8e50fd4f
Nicolas <vazqueznicolasdev@gmail.com>
2022-10-13 15:50:43 -0300

    Create new entry - backend

--- models/Entry.ts
+++ models/Entry.ts
@@ -5,13 +5,14 @@ export interface IEntry extends Entry {}
 
 const entrySchema = new Schema({
 	description: { type: String, required: true },
-	createdAt: { type: Number, required: true },
+	createdAt: { type: Number },
 	status: {
 		type: String,
 		enum: {
 			values: ["pending", "in-progress", "finished"],
 			message: "{VALUE} is not an allowed status",
 		},
+		default: "pending",
 	},
 });
 
--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { db } from "../../../database";
 import { Entry, IEntry } from "../../../models";
 
-type Data = { message: string } | IEntry[];
+type Data = { message: string } | IEntry[] | IEntry;
 
 export default function handler(
 	req: NextApiRequest,
@@ -11,6 +11,9 @@ export default function handler(
 	switch (req.method) {
 		case "GET":
 			return getEntries(res);
+
+		case "POST":
+			return postEntry(req, res);
 
 		default:
 			return res.status(400).json({ message: "The endpoint doesn't exist" });
@@ -23,4 +26,27 @@ const getEntries = async (res: NextApiResponse<Data>) => {
 	await db.disconnect();
 
 	res.status(200).json(entries);
+};
+
+const postEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+	const { description = "" } = req.body;
+
+	const newEntry = new Entry({
+		description,
+		createdAt: Date.now(),
+	});
+
+	try {
+		await db.connect();
+		await newEntry.save();
+		await db.disconnect();
+
+		return res.status(201).json(newEntry);
+	} catch (error) {
+		await db.disconnect();
+		console.log(error);
+		return res
+			.status(500)
+			.json({ message: "Something went wrong, please check the server console." });
+	}
 };

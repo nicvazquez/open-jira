4d38ad2cb6104725bfc63ea442716a59acb4b3f7
Nicolas <vazqueznicolasdev@gmail.com>
2022-10-13 16:12:50 -0300

    PUT - Entry

---
+++ pages/api/entries/[id].ts
@@ -0,0 +1,53 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import mongoose from "mongoose";
+import { db } from "../../../database";
+import { Entry, IEntry } from "../../../models";
+
+type Data = { message: string } | IEntry;
+
+export default function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
+) {
+	const { id } = req.query;
+
+	if (!mongoose.isValidObjectId(id)) {
+		return res.status(400).json({ message: "Invalid ID " + id });
+	}
+
+	switch (req.method) {
+		case "PUT":
+			return updateEntry(req, res);
+
+		default:
+			return res.status(400).json({ message: "Invalid method " });
+	}
+}
+
+const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+	const { id } = req.query;
+
+	await db.connect();
+
+	const entryToUpdate = await Entry.findById(id);
+
+	if (!entryToUpdate) {
+		await db.disconnect();
+		return res
+			.status(400)
+			.json({ message: "There is no entry with this ID " + id });
+	}
+
+	const {
+		description = entryToUpdate.description,
+		status = entryToUpdate.status,
+	} = req.body;
+
+	const updatedEntry = await Entry.findByIdAndUpdate(
+		id,
+		{ description, status },
+		{ runValidators: true, new: true }
+	);
+
+	res.status(200).json(updatedEntry!);
+};

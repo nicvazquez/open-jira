1d61fb991a29f8d2a70a2a98458d6b5e0dad3b63
Nicolas <vazqueznicolasdev@gmail.com>
2022-10-13 15:26:31 -0300

    Read entries - Backend

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,7 +1,8 @@
-import { FC, useReducer } from "react";
+import { FC, useEffect, useReducer } from "react";
 import { Entry } from "../../interfaces";
 import { EntriesContext, entriesReducer } from "./";
 import { v4 as uuidv4 } from "uuid";
+import { entriesApi } from "../../apis";
 
 export interface EntriesState {
 	entries: Entry[];
@@ -32,6 +33,15 @@ export const EntriesProvider: FC<Props> = ({ children }) => {
 	const updateEntry = (entry: Entry) => {
 		dispatch({ type: "[Entry] - Entry-Updated", payload: entry });
 	};
+
+	const refreshEntries = async () => {
+		const { data } = await entriesApi.get<Entry[]>("/entries");
+		dispatch({ type: "[Entry] - Refresh-Data", payload: data });
+	};
+
+	useEffect(() => {
+		refreshEntries();
+	}, []);
 
 	return (
 		<EntriesContext.Provider
